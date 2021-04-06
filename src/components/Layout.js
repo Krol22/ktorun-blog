7d693aa661250f38c21903458b08b8e0a7638e02
Karol Torun <krol22ee@gmail.com>
2021-04-06 23:08:48 +0200

    Adding nvim-frontend-setup post

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,6 +14,10 @@ const Layout = ({ title, description = "", children }) => (
     <Helmet>
       <title>{title} - K.Torun</title>
       <meta name="description" content={description} />
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦄</text></svg>"
+      />
     </Helmet>
     <hr />
     <main>{children}</main>
